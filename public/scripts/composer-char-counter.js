e8a353c3bb979810eb1fff087fbba5c83a8f9d04
David Onaolapo <talk2dawich@gmail.com>
2021-07-13 20:11:48 +0000

    Added counter functionality

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,20 @@
+
+$(document).ready(function() {
+  const counterDisplay = $("#tweet-text").closest("#form").find(".counter")
+  const currentMax = counterDisplay.text();      // Grabs current number written in the html output tag with class .counter
+  const tweeting = $("#tweet-text")
+
+  $(tweeting).keypress(function() {   // count characters written in form and output count of how many left
+    const input = $(this).val().length;  
+    let counter = (currentMax - 1) - input;
+
+    document.getElementById("tweet-counter").innerHTML = counter;
+  });
+
+  // $(tweeting).keydown(function(e) {
+
+  //   if( e.which == 8 || e.which == 46 )
+  //     counter += 1;
+  // });  
+});
+

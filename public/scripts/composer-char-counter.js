a760a97333c6211bd08df6fc700dc5c43ea632fb
David Onaolapo <talk2dawich@gmail.com>
2021-07-14 00:16:30 +0000

    Added article element for tweet display

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,7 +2,7 @@
 $(document).ready(function() {
   const counterDisplay = $("#tweet-text").closest("#form").find(".counter")
   const currentMax = counterDisplay.text();      // Grabs current number written in the html output tag with class .counter
-  const tweeting = $("#tweet-text")
+  const tweeting = $("#tweet-text");
 
   $(tweeting).keypress(function() {   // count characters written in form and output count of how many left
     const input = $(this).val().length;  
@@ -15,6 +15,13 @@ $(document).ready(function() {
     }
   });
 
+  // $("article").mouseover(function() {
+  //   $(this).css("box-shadow", "5px 5px #B0C4DE")
+  // })
+
+  // $("article").mouseleave(function() {
+  //   $(this).css("box-shadow", "0px 0px")
+  // })
   // $(tweeting).keydown(function(e) {
 
   //   if( e.which == 8 || e.which == 46 )

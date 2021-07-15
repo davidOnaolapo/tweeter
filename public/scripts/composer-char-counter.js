9d2c65bb81c38978fb2b5dfee42ec5ccb2c2bfa7
David Onaolapo <talk2dawich@gmail.com>
2021-07-15 01:59:19 +0000

    Counter remains accurate on backspace

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -8,21 +8,21 @@ $(document).ready(function() {
     const input = $(this).val().length;  
     let counter = (currentMax - 1) - input;
 
-    // $(tweeting).keydown(function(e) {
-    //   console.log($(this).val().length)
+    $(tweeting).keydown(function(e) {
+      console.log($(this).val().length)
 
-    //   if(e.which == 8  && counter < 140) {
-    //     counter += 1;
-    //   }
+      if(e.which == 8  && counter < 140) {
+        counter += 1;
+      }
 
-    //   if (counter < 1) {                        //colors the counter red when it goes below 1
-    //     counterDisplay.css("color", "red")
-    //   } else {
-    //     counterDisplay.css("color", "rgb(84, 81, 73)")
-    //   }  
-
-    //   document.getElementById("tweet-counter").innerHTML = counter;
-    // });
+      if (counter < 1) {                        //colors the counter red when it goes below 1
+        counterDisplay.css("color", "red")
+      } else {
+        counterDisplay.css("color", "rgb(84, 81, 73)")
+      }  
+                 
+      document.getElementById("tweet-counter").innerHTML = counter;
+    });
     document.getElementById("tweet-counter").innerHTML = counter;
   });
 });
